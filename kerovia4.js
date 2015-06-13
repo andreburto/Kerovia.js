adab507daf7cc227984910a72b25fa5ec71a8b0c
Andrew Burton <tuglyraisin@gmail.com>
2015-06-12 22:01:51 -0500

    Working on POST methods

--- kerovia4.js
+++ kerovia4.js
@@ -12,9 +12,10 @@ var settings = {
 
 function webDisplay(req, res) {
     
-    function handleRequest(req, res) {
+    function handleGetRequest(req, res) {
         var fileName = req.url;
         var mimeType;
+        var statusCode;
         
         if (fileName == '/')
             fileName = settings.default;
@@ -26,20 +27,37 @@ function webDisplay(req, res) {
         if (fileContent == "") {
             fileContent = loadFile(settings.fourohfour);
             mimeType = mimeTypes(settings.fourohfour);
+            statusCode = 404;
         } else {
             mimeType = mimeTypes(fileName);
+            statusCode = 200;
         }
         
         // Log activity
         console.log("Serving: "+fileName+", Type: "+mimeType);
         
         // Return requested file
-        res.writeHead(200, {"Content-Type": mimeType});
+        res.writeHead(statusCode, {"Content-Type": mimeType});
         res.write(fileContent);
         res.end();
     }
     
-    handleRequest(req, res);
+    function handlePostRequest(req, res) {
+        
+    }
+    
+    switch(req.method) {
+        case "GET":
+            handleGetRequest(req, res);
+            break;
+        case "POST":
+            
+            break;
+        default:
+        res.writeHead(500, {"Content-Type": "text/plain"});
+        res.write("500 Error");
+        res.end();
+    }
 };
 
 console.log("Starting server on port "+settings.port+".");
